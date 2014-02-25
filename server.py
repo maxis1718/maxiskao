ebed2565762079b60af1ae3a7ef4d69276289307
Maxis <maxis1718@gmail.com>
2014-02-26 02:34:33 +0800

    stable version v1.0

--- server.py
+++ server.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 
 pages = ['profile', 'projects', 'publications', 'cv', 'about']
 
+meta = {}
+meta['Keywords'] = ['NTHU,清華大學,自然語言,NLP,Maxis,Kao,高定慧']
+meta['Description'] = "Maxis Kao's personal website."
+meta['Date'] = '2014-02-26'
+meta['Author'] = 'Maxis Kao'
+
 
 def current(target):
 	selected = dict([(p, '' if p != target else 'selected' ) for p in pages])
@@ -19,29 +25,20 @@ def current(target):
 
 @app.route('/')
 @app.route('/<section>')
-@app.route('/<section>/')
 def show(section='profile'):
 	selected, triangle = current(section)
 
 	if section == 'projects':
 		projects = util.build_projects(projects_xml='static/projects.xml', people_xml='static/people.xml')
-		return render_template(section+'.html', selected=selected, triangle=triangle, projects=projects)
+		return render_template(section+'.html', meta=meta, selected=selected, triangle=triangle, projects=projects)
 
 	if section not in pages: 
 		return redirect(url_for('show'))
-		
+
 	else: 
 		return render_template(section+'.html', selected=selected, triangle=triangle)
 		
 
 if __name__ == "__main__":
 	app.debug = True
-	
-	# with app.test_request_context():
-		# print url_for('static')
-		# print url_for('show_projects')
-	# 	print url_for('show_publications')
-	# 	print url_for('show_about')
-	# 	print url_for('statis_img')
-
 	app.run(host="0.0.0.0")
